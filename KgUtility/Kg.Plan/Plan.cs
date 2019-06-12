10cdbf3a5f692289ec4b570d779538095177e4dc
kageyama ginn <hank.han@pery.com>
2019-06-12 16:48:28 +0800

    create KG.plan project and it test project

---
+++ KgUtility/Kg.Plan/Plan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Kg.Plan
+{
+    public class Plan
+    {
+        public static Plan Create
+        {
+            get { return new Plan(); }
+        }
+
+        public void Start()
+        { }
+
+        
+    }
+
+    public static class PlanExten
+    {
+        public static Plan If(this Plan source,string expression)
+        {
+            return source;
+        }
+
+        public static Plan When(this Plan source, string expression)
+        {
+            return source;
+        }
+
+
+        public static Plan After(this Plan source, string expression)
+        {
+            return source;
+        }
+        public static Plan Set(this Plan source, string expression)
+        {
+            return source;
+        }
+        public static Plan Do(this Plan source, string expression)
+        {
+            return source;
+        }
+    }
+}
---
+++ KgUtility/Test_Plan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Kg.Plan;
+
+namespace Test_Plan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plan.Create.
+                Set("").
+                When("result='app'").
+                Do("").
+                Start();
+        }
+
+        static void TestMethod()
+        {
+
+        }
+    }
+}
